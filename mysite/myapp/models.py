b979634273a75e1c83ac2e5b3e63e9dd813be673
Tim Robles <robles.tim@gmail.com>
2022-11-29 21:08:08 -0500

    Final

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,6 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+class Persona(models.Model):
+    # id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Persona'
+        verbose_name_plural = 'Personas'
+        abstract = True
+
+    @property
+    def full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def __str__(self):
+        return self.full_name
+
 class Salon(models.Model):
     # id = models.AutoField(primary_key=True)
     aula = models.CharField(max_length=2)
@@ -15,10 +32,17 @@ class Salon(models.Model):
         ]
 
 
-class Alumno(models.Model):
-    # id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+class Profesor(Persona):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields = ['first_name', 'last_name'], name = 'primary_key_profesor'
+            )
+        ]
+
+
+
+class Alumno(Persona):
     idSalon = models.ForeignKey(Salon, on_delete=models.CASCADE)
 
     class Meta:
@@ -30,15 +54,11 @@ class Alumno(models.Model):
             )
         ]
 
-    @property
-    def full_name(self):
-        return self.first_name + " " + self.last_name
-
-    def __str__(self):
-        return self.full_name
-
 
 class OrderedAlum(Alumno):
     class Meta:
-        ordering = ["last_name"]
+        ordering = [
+            "first_name",
+            "-last_name",
+            ]
         proxy = True
