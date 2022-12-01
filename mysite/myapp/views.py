fe8545d59712ac59f977181154009c435c7c799c
Tim Robles <robles.tim@gmail.com>
2022-11-30 21:11:16 -0500

    Formulario 2

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,8 @@
+from urllib import request
 from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 from myapp.forms import InputForm
 
@@ -37,18 +39,45 @@ class index_view(View):
         return HttpResponse("Index POST")
 
 
-class form_view(View):
-    template = "form.html"
-    contexto = {}
-    contexto['form'] = InputForm()
+# class form_view(View):
+#     template = "form.html"
+#     contexto = {}
 
-    def get(self, request):
-        return render(
-            request=request,
-            template_name=self.template,
-            context=self.contexto
-            )
+#     def get(self, request):
+#         form_get = InputForm(request.GET)
+#         form_post = InputForm(request.POST)
+#         self.contexto['form_get'] = form_get
+#         self.contexto['form_post'] = form_post
+#         return render(
+#             request=request,
+#             template_name=self.template,
+#             context=self.contexto
+#             )
 
-    def post(self, request):
-        print("POST")
-        return HttpResponse("Index POST")
+#     def post(self, request):
+#         print("POST")
+#         form_get = InputForm(request.GET)
+#         form_post = InputForm(request.POST)
+#         self.contexto['form_get'] = form_get
+#         self.contexto['form_post'] = form_post
+#         print(form_post.cleaned_data['aula'])
+#         return render(
+#             request=request,
+#             template_name=self.template,
+#             context=self.contexto
+#             )
+
+def aula_view(request, aula, horario):
+    return HttpResponse(f"El parámetro enviado por URL es {aula}, {horario}")
+
+
+def form_view(request):
+    if request.method == "POST":
+        form = InputForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data["aula"])
+            return redirect(aula_view, aula= form.cleaned_data["aula"], horario=form.cleaned_data["hora_entrada"])
+
+    context = {}
+    context['form']= InputForm()
+    return render(request, "form.html", context)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from myapp.views import form_view, index_view
+from myapp.views import aula_view, form_view, index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path("", index_view, name="home"),
     path("", index_view.as_view(), name="home"),
-    path("formulario/", form_view.as_view(), name="formulario"),
+    # path("formulario/", form_view.as_view(), name="formulario"),
+    path("formulario/", form_view, name="formulario"),
+    path("formulario/<aula>/<horario>", aula_view, name="aula")
 ]
